bc25013885b4230ef2b21b26f593593033b5d174
fede2001 <fede2001@hotmail.com>
2015-04-27 17:59:04 -0500

    Agrego Activities para ir metiendo lo de helena

---
+++ api/controllers/ActivitiesController.js
@@ -0,0 +1,11 @@
+/**
+ * ActivitiesController
+ *
+ * @description :: Server-side logic for managing activities
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
