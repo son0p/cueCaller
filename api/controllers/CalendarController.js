dbdf621c6a3b02a3c881e5658ad4535a3cb15462
fede2001 <fede2001@hotmail.com>
2015-08-12 13:10:02 -0500

    Ensayando como leer los totales de horas de profesores desde al api de google calendar

---
+++ api/controllers/CalendarController.js
@@ -0,0 +1,29 @@
+/**
+ * CalendarController
+ *
+ * @description :: Server-side logic for managing calendars
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+var fs = require('fs');
+var readline = require('readline');
+module.exports = {
+  totalProfes: function(req, res){
+
+    // Load client secrets from a local file.
+    fs.readFile('api/services/client_secret.json', function processClientSecrets(err, content) {
+      if (err) {
+        console.log('Error loading client secret file: ' + err);
+        return;
+      }
+
+      // Authorize a client with the loaded credentials, then call the
+      // Google gCalendar API.
+      var horas = horasProfes.listEvents;
+      var autorizaHoras = horasProfes.authorize(JSON.parse(content),horas );
+
+
+    });
+
+
+    }
+};
---
+++ api/services/horasProfes.js
@@ -0,0 +1,160 @@
+var fs = require('fs');
+var readline = require('readline');
+var google = require('googleapis');
+var googleAuth = require('google-auth-library');
+var S = require('string');
+//var _= require('lodash');
+
+var SCOPES = ['https://www.googleapis.com/auth/calendar.readonly'];
+var TOKEN_DIR = (process.env.HOME || process.env.HOMEPATH ||
+    process.env.USERPROFILE) + '/.credentials/';
+var TOKEN_PATH = TOKEN_DIR + 'calendar-api-quickstart.json';
+
+
+// // Load client secrets from a local file.
+// fs.readFile('api/services/client_secret.json', function processClientSecrets(err, content) {
+//   if (err) {
+//     console.log('Error loading client secret file: ' + err);
+//     return;
+//   }
+//   // Authorize a client with the loaded credentials, then call the
+//   // Google gCalendar API.
+//   module.exports.authorize(JSON.parse(content), module.exports.listEvents);
+// });
+
+/**
+ * Create an OAuth2 client with the given credentials, and then execute the
+ * given callback function.
+ *
+ * @param {Object} credentials The authorization client credentials.
+ * @param {function} callback The callback to call with the authorized client.
+ */
+module.exports = {
+authorize:function (credentials, callback) {
+  var clientSecret = credentials.installed.client_secret;
+  var clientId = credentials.installed.client_id;
+  var redirectUrl = credentials.installed.redirect_uris[0];
+  var auth = new googleAuth();
+  var oauth2Client = new auth.OAuth2(clientId, clientSecret, redirectUrl);
+
+  // Check if we have previously stored a token.
+  fs.readFile(TOKEN_PATH, function(err, token) {
+    if (err) {
+      getNewToken(oauth2Client, callback);
+    } else {
+      oauth2Client.credentials = JSON.parse(token);
+      callback(oauth2Client);
+    }
+  });
+},
+
+/**
+ * Get and store new token after prompting for user authorization, and then
+ * execute the given callback with the authorized OAuth2 client.
+ *
+ * @param {google.auth.OAuth2} oauth2Client The OAuth2 client to get token for.
+ * @param {getEventsCallback} callback The callback to call with the authorized
+ *     client.
+ */
+ getNewToken : function (oauth2Client, callback) {
+  var authUrl = oauth2Client.generateAuthUrl({
+    access_type: 'offline',
+    scope: SCOPES
+  });
+  console.log('Authorize this app by visiting this url: ', authUrl);
+  var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+  rl.question('Enter the code from that page here: ', function(code) {
+    rl.close();
+    oauth2Client.getToken(code, function(err, token) {
+      if (err) {
+        console.log('Error while trying to retrieve access token', err);
+        return;
+      }
+      oauth2Client.credentials = token;
+      storeToken(token);
+      callback(oauth2Client);
+    });
+  });
+},
+
+/**
+ * Store token to disk be used in later program executions.
+ *
+ * @param {Object} token The token to store to disk.
+ */
+storeToken:function (token) {
+  try {
+    fs.mkdirSync(TOKEN_DIR);
+  } catch (err) {
+    if (err.code != 'EEXIST') {
+      throw err;
+    }
+  }
+  fs.writeFile(TOKEN_PATH, JSON.stringify(token));
+  console.log('Token stored to ' + TOKEN_PATH);
+},
+
+/**
+ * Lists the next 10 events on the user's primary calendar.
+ *
+ * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
+ */
+
+
+  listEvents: function (auth) {
+  var calendar = google.calendar('v3');
+  calendar.events.list({
+    auth: auth,
+    calendarId: '0f0n9volku8u02t6dc185uvv24@group.calendar.google.com',
+    timeMin: (new Date()).toISOString(),
+    maxResults: 10,
+    singleEvents: true,
+    orderBy: 'startTime'
+  }, function(err, response) {
+    if (err) {
+      console.log('The API returned an error: ' + err);
+      return;
+    }
+    var events = response.items;
+    if (events.length == 0) {
+      console.log('No upcoming events found.');
+    } else {
+      console.log('Upcoming 10 events:');
+      var todosLosEventos = "";
+      for (var i = 0; i < events.length; i++) {
+        var event = events[i];
+        var start = event.start.dateTime || event.start.date;
+        //console.log('%s - %s', start, event.summary);
+        //console.log(event);
+        //console.log(event.summary);
+        // mas igual es incluyame a mi mismo en la suma
+        todosLosEventos += " " + event.summary;
+      }
+      //console.log(todosLosEventos);
+      var adri =  S(todosLosEventos).count("Adri");
+      // aca puedo multiplicar por el numero de horas
+      // console.log("Adri = ",adri);
+
+      function cuentaNombres(nombre){
+        return S(todosLosEventos).count(nombre);
+        }
+      var arregloProfes = ["Adri","Ligia","Tiby"];
+      var conteo = _.map(arregloProfes, cuentaNombres);
+      arregloProfes.forEach(function(profe, i){
+       console.log(profe,conteo[i]);
+        fs.appendFile("assets/profes.txt", profe + ","+ conteo[i]+"\n", function(err){
+          if(err) {
+            return console.log(err);
+          }
+
+          console.log("The file was saved!");
+        });
+      });
+      fs.close();
+    }
+  });
+}
+};
